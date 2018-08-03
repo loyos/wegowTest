bda8ba2c03f9a46bae402ffb099c99683c07e9cc
unknown <loyenrique1@gmail.com>
2018-08-04 00:43:46 +0200

    catch

--- controllers/myCtrl.js
+++ controllers/myCtrl.js
@@ -1,6 +1,10 @@
 app.controller("myCtrl", function ($scope, $http, wegowService) {
-    wegowService.http().then(function (response) {
-        $scope.events = response.data.events;
-    });
+    wegowService.http()
+        .then(function (response) {
+            $scope.events = response.data.events;
+        })
+        .catch(function (response) {
+            console.error('Error', response.status, response.data);
+        });
 
 });
